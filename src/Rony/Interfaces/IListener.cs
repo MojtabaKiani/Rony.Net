d142f77a96b4b9f79aa3b78388e3a6bf6ea1fcaf
Mojtaba Kiani <kiani@live.com>
2020-12-31 17:09:35 +0330

    Add IListener

---
+++ src/Rony/Interfaces/IListener.cs
@@ -0,0 +1,19 @@
+﻿using Rony.Models;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Rony.Interfaces
+{
+    public interface IListener : IDisposable
+    {
+        IPAddress Address { get; }
+        int Port { get; }
+        bool Active { get; }
+
+        Task<Message> ReceiveAsync();
+        Task ReplyAsync(string text, object sender);
+        void Stop();
+        void Start();
+    }
+}
