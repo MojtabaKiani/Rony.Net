cf991ae891ab2d55c7815bb858b51976e1bb99bc
Mojtaba Kiani <kiani@live.com>
2020-12-31 18:36:07 +0330

    Add MockServer tests with TcpServer

---
+++ tests/Rony.FunctionalTests/MockTcpServerTests.cs
@@ -0,0 +1,122 @@
+﻿using Rony.Listeners;
+using Rony.Net;
+using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Xunit;
+
+namespace Rony.FunctionalTests
+{
+    public class MockTcpServerTests
+    {
+        [Fact]
+        public async void Server_Should_Return_Correct_Response()
+        {
+            //Arrange
+            using var server = new MockServer(new TcpServer(3000));
+            var request = new byte[] { 1, 2, 3 };
+            using var client = new TcpClient();
+
+            //Act
+            server.Mock.Send(request).Receive(x => new byte[] { x[1], 10, x[2] });
+            server.Start();
+            await client.ConnectAsync(IPAddress.Parse("127.0.0.1"), 3000);
+            using var stream = client.GetStream();
+            await stream.WriteAsync(request, 0, request.Length);
+            var response = new byte[client.ReceiveBufferSize];
+            var bytes = await stream.ReadAsync(response, 0, response.Length);
+            client.Close();
+            server.Stop();
+
+            //Assert
+            Assert.True(response.Take(bytes).SequenceEqual(new byte[] { 2, 10, 3 }));
+        }
+
+        [Fact]
+        public async void Server_Should_Return_Correct_Response_On_Multiple_Requests()
+        {
+            //Arrange
+            using var server = new MockServer(new TcpServer(3000));
+            using var client = new TcpClient();
+
+            //Act
+            server.Mock.Send("123").Receive("321");
+            server.Mock.Send("ABC").Receive("CBA");
+            server.Mock.Send("!@#").Receive("$%^");
+            server.Start();
+            await client.ConnectAsync(IPAddress.Parse("127.0.0.1"), 3000);
+            using var stream = client.GetStream();
+            await stream.WriteAsync("ABC".GetBytes(), 0, 3);
+            var response = new byte[client.ReceiveBufferSize];
+            var bytes = stream.Read(response, 0, response.Length);
+            client.Close();
+            server.Stop();
+
+            //Assert
+            Assert.Equal("CBA", response.Take(bytes).ToArray().GetString());
+        }
+
+        [Fact]
+        public async void Server_Should_Return_Correct_Response_On_Many_Request()
+        {
+            //Arrange
+            using var server = new MockServer(new TcpServer(3000));
+
+            //Act
+            for (int i = 0; i < 10000; i++)
+                server.Mock.Send(i.ToString()).Receive((i + 10000).ToString());
+            server.Start();
+            for (int i = 0; i < 10000; i++)
+            {
+                using var client = new TcpClient();
+                await client.ConnectAsync(IPAddress.Parse("127.0.0.1"), 3000);
+                using var stream = client.GetStream();
+                var response = new byte[client.ReceiveBufferSize];
+                var request = i.ToString().GetBytes();
+                await stream.WriteAsync(request, 0, request.Length);
+                var bytes = stream.Read(response, 0, response.Length);
+                client.Close();
+
+                //Assert
+                Assert.Equal((i + 10000).ToString(), response.Take(bytes).ToArray().GetString());
+            }
+            server.Stop();
+        }
+
+        [Fact]
+        public void Server_Should_Return_Correct_Response_On_Multi_Thread_Request()
+        {
+            //Act
+            for (int i = 0; i < 100; i++)
+            {
+                ThreadPool.QueueUserWorkItem(new WaitCallback(ConnectServer), i.ToString());
+            }
+
+            async void ConnectServer(object input)
+            {
+                var header = (string)input;
+                using var server = new MockServer(new TcpServer(int.Parse(header)));
+                for (int i = 0; i < 3000; i++)
+                    server.Mock.Send($"{header}-{i}").Receive($"{header}-{i + 10000}");
+                server.Start();
+                for (int i = 0; i < 3000; i++)
+                {
+                    var client = new TcpClient();
+                    await client.ConnectAsync(IPAddress.Parse("127.0.0.1"), int.Parse(header));
+                    using var stream = client.GetStream();
+                    var response = new byte[client.ReceiveBufferSize];
+                    var request = $"{header}-{i}".ToString().GetBytes();
+                    await stream.WriteAsync(request, 0, request.Length);
+                    var bytes = stream.Read(response, 0, response.Length);
+                    client.Close();
+
+                    //Assert
+                    Assert.Equal($"{header}-{i + 10000}", response.Take(bytes).ToArray().GetString());
+                }
+                server.Stop();
+            }
+        }
+    }
+}
---
+++ tests/Rony.UnitTests/MockServerTests.cs
@@ -0,0 +1,76 @@
+﻿using Rony.Listeners;
+using Rony.Net;
+using System;
+using Xunit;
+
+namespace Rony.Tests
+{
+    public class MockServerTests
+    {
+        [Fact]
+        public void Constructor_Should_Work_Correctly()
+        {
+            //Arrange
+            using var server = new MockServer(new TcpServer(5000));
+
+            //Assert
+            Assert.NotNull(server);
+            Assert.NotNull(server.Mock);
+            Assert.Equal("127.0.0.1", server.Address.ToString());
+            Assert.Equal(5000, server.Port);
+        }
+
+        [Fact]
+        public void Active_Property_Should_Set_Correctly()
+        {
+            //Arrange
+            using var server = new MockServer(new TcpServer(5001));
+
+            //Act
+            server.Start();
+
+            //Assert
+            Assert.True(server.Active);
+
+            //Act
+            server.Stop();
+
+            //Assert
+            Assert.False(server.Active);
+        }
+
+        [Fact]
+        public void Server_Should_Be_Stop_After_Dispose()
+        {
+            //Arrange
+            using var server = new MockServer(new TcpServer(5001));
+
+            //Act
+            server.Start();
+
+            //Assert
+            Assert.True(server.Active);
+
+            //Act
+            server.Dispose();
+
+            //Assert
+            Assert.False(server.Active);
+
+        }
+
+        [Fact]
+        public void Server_Should_Return_Error_On_Adding_Duplicate_Request()
+        {
+            //Arrange
+            var server = new MockServer(new TcpServer(3000));
+            var request = new byte[] { 1, 2, 3 };
+
+            //Act
+            server.Mock.Send(request).Receive(new byte[] { 3, 4, 5 });
+            
+            //Assert
+            Assert.Throws<ArgumentException>(()=>server.Mock.Send(request).Receive("test"));
+        }
+    }
+}
