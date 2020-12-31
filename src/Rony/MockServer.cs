81e0538c00d168a91238c53294bba1a267cc8e12
Mojtaba Kiani <kiani@live.com>
2020-12-31 17:51:36 +0330

    Add MockServer

---
+++ src/Rony/MockServer.cs
@@ -0,0 +1,62 @@
+﻿using Rony.Handlers;
+using Rony.Interfaces;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Rony.Net
+{
+    public class MockServer : IDisposable
+    {
+        private readonly IListener _listener;
+        private bool _listening = false;
+        private readonly object _syncRoot;
+
+        public IPAddress Address => _listener.Address;
+        public int Port => _listener.Port;
+        public bool Active => _listener.Active;
+        public RequestHandler Mock { get; set; }
+        
+        public MockServer(IListener listener)
+        {
+            _syncRoot = new object();
+            Mock = new RequestHandler();
+            _listener = listener;
+        }
+        public void Start()
+        {
+            lock (_syncRoot)
+            {
+                _listener.Start();
+                _listening = true;
+            }
+
+            Task.Factory.StartNew(async () =>
+            {
+                while (_listening)
+                {
+                    var received = await _listener.ReceiveAsync();
+                    var response = Mock.Match(received.BodyString);
+                    if (response != "")
+                    {
+                        await _listener.ReplyAsync(response, received.Sender);
+                    }
+                }
+            });
+        }
+        public void Stop()
+        {
+            if (!_listening) return;
+            lock (_syncRoot)
+            {
+                _listening = false;
+                _listener.Stop();
+            }
+        }
+
+        public void Dispose()
+        {
+            Stop();
+        }
+    }
+}
