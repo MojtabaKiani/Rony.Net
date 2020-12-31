e0a4c2f191668527851bcd7c701b4b2b9ad265e9
Mojtaba Kiani <kiani@live.com>
2020-12-31 16:51:05 +0330

    Add RequestHandler

---
+++ src/Rony/Handlers/RequestHandler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Rony.Handlers
+{
+    public class RequestHandler
+    {
+        private string _receiveData;
+        public Dictionary<string, string> Configs { get; set; }
+
+        public RequestHandler()
+        {
+            Configs=new Dictionary<string, string>();
+        }
+
+        public RequestHandler Send(string receiveData)
+        {
+            _receiveData = receiveData;
+            return this;
+        }
+
+        public RequestHandler Send(byte[] receiveData)
+        {
+            return Send(receiveData.GetString());
+        }
+
+        public void Receive(string response)
+        {
+            Configs.Add(_receiveData, response);
+        }
+
+        public void Receive(byte[] response)
+        {
+            Receive(response.GetString());
+        }
+
+        public void Receive(Func<string, string> func)
+        {
+            Receive(func(_receiveData));
+        }
+
+        public void Receive(Func<byte[], byte[]> func)
+        {
+            Receive(func(_receiveData.GetBytes()));
+        }
+
+        public string Match(string request)
+        {
+            return Configs.LastOrDefault(x => x.Key == request).Value;
+        }
+    }
+}
---
+++ tests/Rony.UnitTests/Handler/RequestHandlerTests.cs
@@ -0,0 +1,95 @@
+﻿using Rony.Handlers;
+using System;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Rony.Tests.Handler
+{
+    public class RequestHandlerTests
+    {
+        private readonly RequestHandler _handler;
+
+        public RequestHandlerTests()
+        {
+            //Arrange
+            _handler = new RequestHandler();
+        }
+        [Fact]
+        public void Constructor_Should_Work_Correctly()
+        {
+            //Assert
+            Assert.NotNull(_handler.Configs);
+        }
+
+        [Theory]
+        [InlineData("TestData", "TestRecieveData")]
+        [InlineData("1234","2345")]
+        [InlineData("ABCD","EFGHI")]
+        [InlineData("4321","HGHST")]
+        [InlineData(",#$@","*&%$#@")]
+        public void Config_With_String_Should_Add_Config(string request,string response)
+        {
+            //Act
+            _handler.Send(request).Receive(response);
+
+            //Assert
+            Assert.Single(_handler.Configs);
+            Assert.Equal(response, _handler.Match(request));
+        }
+
+        [Theory]
+        [InlineData("TestData", "TestRecieveData")]
+        [InlineData("1234", "2345")]
+        [InlineData("ABCD", "EFGHI")]
+        [InlineData("4321", "HGHST")]
+        [InlineData(",#$@", "*&%$#@")]
+        public void Config_With_Byte_Array_Should_Add_Config(string request, string response)
+        {
+            //Arrange
+            var requestByte = Encoding.UTF8.GetBytes(request);
+            var responseByte = Encoding.UTF8.GetBytes(response);
+            
+            //Act
+            _handler.Send(requestByte).Receive(responseByte);
+            var received = _handler.Match(Encoding.UTF8.GetString(requestByte));
+            var receivedBytes = Encoding.UTF8.GetBytes(received);
+
+            //Assert
+            Assert.Single(_handler.Configs);
+            Assert.True(responseByte.SequenceEqual(receivedBytes));
+        }
+
+        [Theory]
+        [InlineData("TestData", "TESTDATA")]
+        [InlineData("1234", "1234")]
+        [InlineData("ABCD", "ABCD")]
+        [InlineData("asdfgh", "ASDFGH")]
+        public void Config_With_Func_Of_String_Should_Add_Config(string request, string response)
+        {
+            //Act
+            _handler.Send(request).Receive(x=> x.ToUpper());
+
+            //Assert
+            Assert.Single(_handler.Configs);
+            Assert.Equal(response, _handler.Match(request));
+        }
+
+        [Fact]
+        public void Config_With_Func_Of_Byte_Array_Should_Add_Config()
+        {
+            //Arrange
+            var request = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var expectedResponse = new byte[] { 1, 3, 5, 7, 9 };
+
+            //Act
+            _handler.Send(request).Receive((x => x.Select((s,i)=> (s,i)).Where(t=> t.i % 2 ==0).Select(t=> t.s).ToArray()));
+            var received = _handler.Match(Encoding.UTF8.GetString(request));
+            var receivedBytes = Encoding.UTF8.GetBytes(received);
+
+            //Assert
+            Assert.Single(_handler.Configs);
+            Assert.True(expectedResponse.SequenceEqual(receivedBytes));
+        }
+    }
+}
